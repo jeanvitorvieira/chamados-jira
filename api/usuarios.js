c5ab060aac94a9ab92b5793e27fd7a29db808e7c
Jean Vitor Vieira <116529050+jeanvitorvieira@users.noreply.github.com>
2026-06-10 22:48:59 -0300

    Update user name mapping to use email address

--- api/usuarios.js
+++ api/usuarios.js
@@ -48,7 +48,7 @@ module.exports = async function handler(req, res) {
   }
 
   const users = rawUsers.map(u => ({
-    name:        u.name,
+    name:        u.emailAddress || u.name, // usa email como identificador para o JQL
     displayName: u.displayName,
     email:       u.emailAddress,
   }));
